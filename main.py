1306324be7650ac9edad866f5a8ddbc9bad131a4
yasnosolnce2024 <pomogaika51@gmail.com>
2024-03-26 05:52:17 +0700

    main code

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-print('это начало программы')
-
-for i in range(8):
-    print('слово')
-a, v = 9, 8
-
-if a > v:
-     print('а больше')
-elif a < v:
-     print(' v больше')
-else:
-     print('они равны')
+print('это конец программы')
 
